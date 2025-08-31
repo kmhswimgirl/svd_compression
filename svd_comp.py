b885132103091f2fec7e0fbcf750c20dcc26ce5b
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-08-30 22:34:14 -0400

    updated files

--- svd_comp.py
+++ svd_comp.py
@@ -1,1 +1,26 @@
 import cv2
+import numpy as np
+
+class ImageSVD:
+    def __init__(self, image, color):
+        self.image
+        self.color
+    
+    def get_image_data(self, image):
+        pass
+
+    def svd_by_hand():
+        pass
+    
+    def
+
+
+# make args eventually
+# image_name = 'egg_car_v2.jpg'
+# image_data = cv2.imread(image_name)
+
+# split into three color channels + a brightness channel
+
+# perform SVD on each channel
+
+# export 
